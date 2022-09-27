03826d1ccce2f3c193364df9e92a35a55564608e
Kellye Richards <kellye.whitfill@gmail.com>
2022-09-27 09:39:18 -0500

    original upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// Use a console log to have your favorite number display in the console/terminal
+console.log(13);
+
+// Use a comment to type out the 5 main data types (AKA Primitives)
+/*
+Number
+Undefined
+Boolean
+Null
+String
+*/
+
+// Use a comment to type out the 6 operator symbols
+/*
+Addition is +
+Subtract is -
+Division is /
+Multiplication is *
+Modulus is %
+Exponent is **
+*/
+
+// Use modulus to get a remainder of 1 from the number 5.
+console.log(5 % 2); //1
+console.log(5 % 4); //1
+
+// Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console
+console.log('I\'M MARRY POPPINS, Y\'ALL');
+
+// Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
+console.log("Hawk" + "eye");
+
+// Create a string for your first name and also your last name. Use the length property to add them together
+console.log("Kellye".length + "Richards".length);
+
+// Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". 
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\/");
+// 2
+console.log(5 ** 4 - 9);
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// 4
+console.log("CAP"[1]); 
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]); 
+console.log("GROOT"[0]); 
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]); 
+console.log("STAR-LORD"[0]);
+// 5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
